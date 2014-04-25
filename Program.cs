a883fe9aeb9096e8d431bddda74c4256c218d65f
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2014-04-25 19:18:43 +0200

    Re-added System.Diagnostics dependency

--- Program.cs
+++ Program.cs
@@ -24,6 +24,7 @@ namespace ResourceExtractor
 {
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics;
     using System.Diagnostics.CodeAnalysis;
     using System.Dynamic;
     using System.Globalization;

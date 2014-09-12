1b34517dc90ae011a0e12f685e996082a3892cd5
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2014-09-13 00:17:15 +0200

    BitmapParser: Bypassed expensive SetPixel calls

--- BitmapParser.cs
+++ BitmapParser.cs
@@ -30,120 +30,45 @@ namespace ResourceExtractor
     {
         internal static Bitmap Parse(BinaryReader reader, ImageHeader header)
         {
-            var Format = (header.Type == ImageType.DXT2 || header.Type == ImageType.DXT4) ? PixelFormat.Format32bppPArgb : PixelFormat.Format32bppArgb;
+            var Result = new Bitmap(header.Width, header.Height, PixelFormat.Format32bppArgb);
+            var Raw = Result.LockBits(new Rectangle(0, 0, Result.Width, Result.Height), ImageLockMode.WriteOnly, Result.PixelFormat);
 
-            var Result = new Bitmap((int)header.Width, (int)header.Height, Format);
+            var PixelCount = header.Height * header.Width;
 
-            int PixelCount = (int)(header.Height * header.Width);
-            if (header.BitCount == 8)
-            { // 8-bit, with palette
-                Color[] Palette = new Color[256];
-                for (int i = 0; i < 256; ++i)
-                    Palette[i] = ReadColor(reader, 32);
-                byte[] BitFields = reader.ReadBytes(PixelCount);
-                for (int i = 0; i < PixelCount; ++i)
-                {
-                    int x = (int)(i % header.Width);
-                    int y = (int)(header.Height - 1 - ((i - x) / header.Width));
-                    Result.SetPixel(x, y, Palette[BitFields[i]]);
-                }
-            }
-            else
+            unsafe
             {
-                for (int i = 0; i < PixelCount; ++i)
-                {
-                    int x = (i % header.Width);
-                    int y = header.Height - 1 - ((i - x) / header.Width);
-                    Result.SetPixel(x, y, ReadColor(reader, header.BitCount));
-                }
-            }
+                var Buffer = (byte*)Raw.Scan0;
 
-            return Result;
-        }
-
-        private static Color[] ReadTexelBlock(BinaryReader reader, ImageType type)
-        {
-            ulong AlphaBlock = 0;
-
-            if (type == ImageType.DXT2 || type == ImageType.DXT3 || type == ImageType.DXT4 || type == ImageType.DXT5)
-            {
-                AlphaBlock = reader.ReadUInt64();
-            }
-            else if (type != ImageType.DXT1)
-            {
-                throw new InvalidDataException(string.Format("Format {0} not recognized.", type));
-            }
-
-            ushort C0 = reader.ReadUInt16();
-            ushort C1 = reader.ReadUInt16();
-            Color[] Colors = new Color[4];
-            Colors[0] = DecodeRGB565(C0);
-            Colors[1] = DecodeRGB565(C1);
-            if (C0 > C1 || type != ImageType.DXT1)
-            { // opaque, 4-color
-                Colors[2] = Color.FromArgb((2 * Colors[0].R + Colors[1].R + 1) / 3, (2 * Colors[0].G + Colors[1].G + 1) / 3, (2 * Colors[0].B + Colors[1].B + 1) / 3);
-                Colors[3] = Color.FromArgb((2 * Colors[1].R + Colors[0].R + 1) / 3, (2 * Colors[1].G + Colors[0].G + 1) / 3, (2 * Colors[1].B + Colors[0].B + 1) / 3);
-            }
-            else
-            { // 1-bit alpha, 3-color
-                Colors[2] = Color.FromArgb((Colors[0].R + Colors[1].R) / 2, (Colors[0].G + Colors[1].G) / 2, (Colors[0].B + Colors[1].B) / 2);
-                Colors[3] = Color.Transparent;
-            }
-            uint CompressedColor = reader.ReadUInt32();
-            Color[] DecodedColors = new Color[16];
-            for (int i = 0; i < 16; ++i)
-            {
-                if (type == ImageType.DXT2 || type == ImageType.DXT3 || type == ImageType.DXT4 || type == ImageType.DXT5)
-                {
-                    int A = 255;
-                    if (type == ImageType.DXT2 || type == ImageType.DXT3)
+                Color[] Palette = null;
+                byte[] BitFields = null;
+                bool EightCount = header.BitCount == 8;
+                if (EightCount)
+                { // 8-bit, with palette
+                    Palette = new Color[256];
+                    for (var i = 0; i < 256; ++i)
                     {
-                        // Seems to be 8 maximum; so treat 8 as 255 and all other values as 3-bit alpha
-                        A = (int)((AlphaBlock >> (4 * i)) & 0xF);
-                        if (A >= 8)
-                        {
-                            A = 0xFF;
-                        }
-                        else
-                        {
-                            A <<= 5;
-                        }
+                        Palette[i] = ReadColor(reader, 32);
                     }
-                    else
-                    { // Interpolated alpha
-                        int[] Alphas = new int[8];
-                        Alphas[0] = (byte)((AlphaBlock >> 0) & 0xFF);
-                        Alphas[1] = (byte)((AlphaBlock >> 8) & 0xFF);
-                        if (Alphas[0] > Alphas[1])
-                        {
-                            Alphas[2] = (Alphas[0] * 6 + Alphas[1] * 1 + 3) / 7;
-                            Alphas[3] = (Alphas[0] * 5 + Alphas[1] * 2 + 3) / 7;
-                            Alphas[4] = (Alphas[0] * 4 + Alphas[1] * 3 + 3) / 7;
-                            Alphas[5] = (Alphas[0] * 3 + Alphas[1] * 4 + 3) / 7;
-                            Alphas[6] = (Alphas[0] * 2 + Alphas[1] * 5 + 3) / 7;
-                            Alphas[7] = (Alphas[0] * 1 + Alphas[1] * 6 + 3) / 7;
-                        }
-                        else
-                        {
-                            Alphas[2] = (Alphas[0] * 4 + Alphas[1] * 1 + 2) / 5;
-                            Alphas[3] = (Alphas[0] * 3 + Alphas[1] * 2 + 2) / 5;
-                            Alphas[4] = (Alphas[0] * 2 + Alphas[1] * 3 + 2) / 5;
-                            Alphas[5] = (Alphas[0] * 1 + Alphas[1] * 4 + 2) / 5;
-                            Alphas[6] = 0;
-                            Alphas[7] = 255;
-                        }
-                        ulong AlphaMatrix = (AlphaBlock >> 16) & 0xFFFFFFFFFFFFL;
-                        A = Alphas[(AlphaMatrix >> (3 * i)) & 0x7];
-                    }
-                    DecodedColors[i] = Color.FromArgb(A, Colors[CompressedColor & 0x3]);
+
+                    BitFields = new byte[PixelCount];
+                    var PixelStride = PixelCount / Result.Width;
+                    BitFields = reader.ReadBytes(PixelCount);
                 }
-                else
+
+                for (var Pixel = 0; Pixel < PixelCount; ++Pixel)
                 {
-                    DecodedColors[i] = Colors[CompressedColor & 0x3];
+                    var Color = EightCount ? Palette[BitFields[Pixel]] : ReadColor(reader, header.BitCount);
+                    Buffer[4 * Pixel + 0] = Color.B;
+                    Buffer[4 * Pixel + 1] = Color.G;
+                    Buffer[4 * Pixel + 2] = Color.R;
+                    Buffer[4 * Pixel + 3] = Color.A;
                 }
-                CompressedColor >>= 2;
             }
-            return DecodedColors;
+
+            Result.UnlockBits(Raw);
+
+            Result.RotateFlip(RotateFlipType.RotateNoneFlipY);
+            return Result;
         }
 
         private static Color DecodeRGB565(ushort C)
@@ -151,28 +76,30 @@ namespace ResourceExtractor
             return Color.FromArgb((C & 0xF800) >> 8, (C & 0x07E0) >> 3, (C & 0x001F) << 3);
         }
 
-        public static Color ReadColor(BinaryReader BR, int BitDepth)
+        public static Color ReadColor(BinaryReader reader, int bitDepth)
         {
-            switch (BitDepth)
+            switch (bitDepth)
             {
             case 8:
                 {
-                    int GrayScale = BR.ReadByte();
+                    int GrayScale = reader.ReadByte();
                     return Color.FromArgb(GrayScale, GrayScale, GrayScale);
                 }
-            case 16: return DecodeRGB565(BR.ReadUInt16());
+            case 16: return DecodeRGB565(reader.ReadUInt16());
             case 24:
             case 32:
                 {
-                    int B = BR.ReadByte();
-                    int G = BR.ReadByte();
-                    int R = BR.ReadByte();
+                    int B = reader.ReadByte();
+                    int G = reader.ReadByte();
+                    int R = reader.ReadByte();
                     int A = 255;
-                    if (BitDepth == 32)
+                    if (bitDepth == 32)
                     {
-                        byte SemiAlpha = BR.ReadByte();
+                        byte SemiAlpha = reader.ReadByte();
                         if (SemiAlpha < 0x80)
+                        {
                             A = 2 * SemiAlpha;
+                        }
                     }
                     return Color.FromArgb(A, R, G, B);
                 }
